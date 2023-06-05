49c2b4eb33fc028c7d9f626ed8ea54c84e19b918
unknown <ahz45bny@gmail.com>
2023-06-05 09:57:30 +0200

    Add test folder

---
+++ tests/test_main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+/* Format to use for tests*/
+// Test class X
+// void testX(){
+//     X x = X()
+//     int result = X.add(4, 6)
+//     cout << "Resultat addition:" << result << "\n"
+// }
+/* Format to use for tests*/
+
+int main(void)
+{
+    /* Format to use for tests*/
+    // testX();
+    /* Format to use for tests*/
+}
