22fed2fef8b61e768e50b5f883c3f465374677cd
adjitoure <124590246+adjitoure@users.noreply.github.com>
2023-06-10 23:32:11 +0200

    Update gameController.cpp
    r

--- gameController.cpp
+++ gameController.cpp
@@ -1,6 +1,6 @@
 #include "gameController.h"
-#include <iostream>
 #include <chrono>
+#include <iostream>
 #include <thread>
 using namespace std;
 
@@ -13,7 +13,7 @@ gameController::gameController(void) {
   this->board = Board();
   this->playersAMove = {
       player1.getAnnouncedMove(),
-      player2.getAnnouncedMove(),                                                                                                               
+      player2.getAnnouncedMove(),
       player3.getAnnouncedMove(),
       player4.getAnnouncedMove(),
   };
@@ -29,14 +29,17 @@ list<int> gameController::getList(void) const { return playersAMove; }
 
 // Methodes
 void gameController::launchGame(void) {
+  std::srand(std::time(NULL));
   Board NewBoard = getBoard();
   Player p1 = getPlayer1();
   Player p2 = getPlayer2();
   Player p3 = getPlayer3();
   Player p4 = getPlayer4();
   // choix de l'objectif
-  
-  
+  Tile tileToReach = Tile();
+  tileToReach.setColor(static_cast<Color>(rand() % 5));
+  tileToReach.setShape(static_cast<Shape>(rand() % 4));
+  NewBoard.renderBoard();
 }
 void gameController::getInput(void) {
   list<int> List = getList();
@@ -44,19 +47,19 @@ void gameController::getInput(void) {
   int min = *List.begin();
   // Durée totale du chronomètre en secondes
   int dureeChronometre = 60;
-    
+
   auto startTime = std::chrono::steady_clock::now();
   auto endTime = startTime + std::chrono::seconds(dureeChronometre);
-    
+
   while (std::chrono::steady_clock::now() < endTime) {
-      std::chrono::duration<double> tempsRestant = endTime -   std::chrono::steady_clock::now();
-      int secondesRestantes = static_cast<int>(tempsRestant.count());
-        
-      std::cout << "Temps restant : " << secondesRestantes << " secondes" << std::endl;
-      std::this_thread::sleep_for(std::chrono::milliseconds(500));
-    }
+    std::chrono::duration<double> tempsRestant =
+        endTime - std::chrono::steady_clock::now();
+    int secondesRestantes = static_cast<int>(tempsRestant.count());
+
+    std::cout << "Temps restant : " << secondesRestantes << " secondes"
+              << std::endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+  }
 
   std::cout << "Le chronomètre est terminé !" << std::endl;
-  
-  
 }
